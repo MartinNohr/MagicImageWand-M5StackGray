f058ee84c9ab355bb01903e249a415ee2d92b133
Martin Nohr <martin.nohr@nohrphoto.com>
2021-04-16 19:59:40 -0600

    Changed LED pin numbers.

--- MagicImageWand-M5StackGray.h
+++ MagicImageWand-M5StackGray.h
@@ -18,8 +18,8 @@ RTC_DATA_ATTR int nColumnHoldTime = 10;                 // mSec frame hold time
 RTC_DATA_ATTR bool bFixedTime = false;                  // fixed total time or hold time for image
 RTC_DATA_ATTR int nFixedImageTime = 10;                 // seconds of time
 // led strip settings
-#define DATA_PIN1 21
-#define DATA_PIN2 22
+#define DATA_PIN1 2
+#define DATA_PIN2 5
 CRGB* leds;
 RTC_DATA_ATTR int nLEDBrightness = 25;                  // 255 is 100%
 RTC_DATA_ATTR bool bSecondController = false;
